ce0b39ea1f3d0aedcb0a81c1e6730f2141fb8c7f
LAPTOP-G8OAT0I5\arman <armandojaleo@gmail.com>
2026-01-20 10:49:52 +0100

    restauracion

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'b1d89095368eaad4a31e1c29723be3c62fb344a4',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'b1d89095368eaad4a31e1c29723be3c62fb344a4',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'cloudstek/php-laff' => array(
+            'pretty_version' => 'v1.1.1',
+            'version' => '1.1.1.0',
+            'reference' => '41f9d0f328d8ac91b18d7b931597f876b1244401',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../cloudstek/php-laff',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
